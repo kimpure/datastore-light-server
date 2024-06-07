4b644bb27bf5b117528f168f83210ffe10b1f2a7
선우 <robloxstudiopure@gmail.com>
2024-06-08 05:07:00 +0900

    Make ClearData (func) , DiscardChanges (func) , BeforeData (obj)

--- server/DataStoreLight.lua
+++ server/DataStoreLight.lua
@@ -7,6 +7,7 @@ local msgpack = require(ReplicatedStorage:WaitForChild('msgpack-luau'))
 
 local PlayerData1 = {}
 local PlayerData2 = {}
+local PlayerBeforeDataInServer = {}
 
 local DataStoreLight = {}
 DataStoreLight.__index = DataStoreLight
@@ -21,13 +22,14 @@ DataStoreLight.Setting = {
 }
 
 
---// Debug
+--// DataStoreLight.Setting.Debug 가 true 일떄 파라미터들을 출력
 function DataStoreLight:Debug(message , ...)
     if self.Setting.Debug then
         print(message , ...)
     end
 end
 
+--// 세션 회수가 초과했다면 기다림
 function DataStoreLight:waitForRequest(requestType)
     local current = DataStoreService:GetRequestBudgetForRequestType(requestType)
 	self:Debug(`남은 새션개수 : {current}`)
@@ -37,6 +39,7 @@ function DataStoreLight:waitForRequest(requestType)
     end
 end
 
+--// Player 의 Data 를 DataStoreLight.DataTable 에 값에 있는 값들마다 로드
 function DataStoreLight.Load(Player:Player)
     for k,v in DataStoreLight.DataTable do
         DataStoreLight:Debug(`DataName : {k} , Player User Id : {Player.UserId}`)
@@ -45,6 +48,7 @@ function DataStoreLight.Load(Player:Player)
         
         PlayerData1[Player.UserId] = {}
         PlayerData2[Player.UserId] = {}
+        PlayerBeforeDataInServer[Player.UserId] = {}
 
         local K1 = DataStoreService:GetDataStore(`{k}:1`):GetAsync(Player.UserId)
         local K2 = DataStoreService:GetDataStore(`{k}:2`):GetAsync(Player.UserId)
@@ -56,7 +60,7 @@ function DataStoreLight.Load(Player:Player)
             local _o = string.sub(K1 , 1 , 1)
             K1 = string.sub(K1 , 2)
 
-            if _o == '' then
+            if _o == '\30' then
                 DataStoreLight:Debug(`압축돼지 않은 데이터 : {K1}`)
 
                 K1 = msgpack.decode(msgpack.utf8Decode(K1))
@@ -80,7 +84,7 @@ function DataStoreLight.Load(Player:Player)
             local _o = string.sub(K2 , 1 , 1)
             K2 = string.sub(K2 , 2)
 
-            if _o == '' then
+            if _o == '\30' then
                 DataStoreLight:Debug(`압축돼지 않은 데이터 : {K2}`)
 
                 K2 = msgpack.decode(msgpack.utf8Decode(K2))
@@ -95,6 +99,7 @@ function DataStoreLight.Load(Player:Player)
     end
 end
 
+--// DataStoreLight 의 PlayerData 라는 저장돼어있는 테이블에서 Data 를 가져옴
 function DataStoreLight.Get(Player:Player , DataName:string) : {any}
     if PlayerData1[Player.UserId] then
         if PlayerData1[Player.UserId][DataName] then
@@ -103,15 +108,18 @@ function DataStoreLight.Get(Player:Player , DataName:string) : {any}
     end
 end
 
+--// DataStoreLight 의 PlayerData 라는 저장돼는 테이블의 DataName 을 Value 의 값으로 변경
 function DataStoreLight.Set<T>(Player:Player , DataName:string , Value:T)
     if PlayerData1[Player.UserId] then
         if PlayerData1[Player.UserId][DataName] then
+            PlayerBeforeDataInServer[Player.UserId][DataName] = PlayerData2[Player.UserId][DataName]
             PlayerData2[Player.UserId][DataName] = PlayerData1[Player.UserId][DataName]
             PlayerData1[Player.UserId][DataName] = Value
         end
     end
 end
 
+--// DataStoreLight.DataTable 에 있는 Data 들을 하나하나 모두 저장
 function DataStoreLight.Save(Player:Player)
     for k,v in DataStoreLight.DataTable do
         
@@ -132,7 +140,7 @@ function DataStoreLight.Save(Player:Player)
                 
                 DataStoreLight:waitForRequest(Enum.DataStoreRequestType.SetIncrementAsync)
                 
-                DataStoreService:GetDataStore(`{k}:1`):SetAsync(Player.UserId , `\30 {base91.encodeString(Compress_Data1)}`)
+                DataStoreService:GetDataStore(`{k}:1`):SetAsync(Player.UserId , `\30{base91.encodeString(Compress_Data1)}`)
             else
                 DataStoreLight:Debug(`압축하지 못한 데이터 저장중 : {Data1} ...`)
                 
@@ -165,5 +173,48 @@ function DataStoreLight.Save(Player:Player)
     end
 end
 
+--// DataName 파라미터가 존제한다면 그 데이터를 초기값으로 만약 존제하지 않는다면 모든 데이터를 초기값으로
+function DataStoreLight.ClearData(Player:Player , DataName:string)
+    if DataName then
+        if DataStoreLight.DataTable[DataName] then
+            if PlayerData1[Player.UserId] then
+                if PlayerData1[Player.UserId][DataName] then
+                    DataStoreLight.Set(Player , DataName , DataStoreLight.DataTable[DataName])
+                end
+            end
+        end
+    else
+        for k,v in DataStoreLight.DataTable do
+            if PlayerData1[Player.UserId] then
+                if PlayerData1[Player.UserId][k] then
+                    DataStoreLight.Set(Player , DataName , v)
+                end
+            end
+        end
+    end
+end
+
+--// DataName 파라미터가 존제하면 그 값만 이전값으로 변경 없다면 모든값을 이전값으로 변경
+function DataStoreLight.DiscardChanges(Player:Player , DataName:string)
+    if DataName then
+        if DataStoreLight.DataTable[DataName] then
+            if PlayerData1[Player.UserId] then
+                if PlayerData1[Player.UserId][DataName] then
+                    PlayerData1[Player.UserId][DataName] = PlayerData2[Player.UserId][DataName]
+                    PlayerData2[Player.UserId][DataName] = PlayerBeforeDataInServer[Player.UserId][DataName]
+                end
+            end
+        end
+    else
+        for k,_ in DataStoreLight.DataTable do
+            if PlayerData1[Player.UserId] then
+                if PlayerData1[Player.UserId][k] then
+                    PlayerData1[Player.UserId][k] = PlayerData2[Player.UserId][k]
+                    PlayerData2[Player.UserId][k] = PlayerBeforeDataInServer[Player.UserId][k]
+                end
+            end
+        end
+    end
+end
 
 return DataStoreLight
